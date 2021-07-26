ceffcf054645cc6e06d8f1982ba4094b68e7f4b7
Avircut <lomoff.slaw@yandex.ru>
2021-07-26 11:57:26 +0700

    Project Structure built

---
+++ TaskManager/Data/Interfaces/ITasks.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManager.Data.Models;
+
+namespace TaskManager.Data.Interfaces
+{
+    interface ITasks
+    {
+        IEnumerable<Tasks> GetTasks();
+    }
+}
--- TaskManager/Startup.cs
+++ TaskManager/Startup.cs
@@ -1,20 +1,32 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TaskManager.DAL;
 
 namespace TaskManager
 {
     public class Startup
     {
+        private readonly IConfigurationRoot _confString;
+        public Startup(IHostingEnvironment env)
+        {
+            _confString = new ConfigurationBuilder().SetBasePath(env.ContentRootPath).AddJsonFile("dbsettings.json").Build();
+        }
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TaskDbContext>(options => options.UseSqlServer(_confString.GetConnectionString("DefaultConnection")));
+            services.AddTransient<ICategory, CategoryRepository>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +36,15 @@ namespace TaskManager
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
